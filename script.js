ca66a3576c3114f0baa5c59bdd2457b8fde7f53e
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2024-10-11 23:51:17 -0600

    Generar casillas

--- script.js
+++ script.js
@@ -11,7 +11,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Agrega un listener para el evento 'input'
     $tamanoArreglo.addEventListener('input', function() {
-        const $tamano = $tamanoArreglo.value;
-        console.log($tamano);
+        // parseInt() convierte un string en un número entero
+        const tamano = parseInt($tamanoArreglo.value, 10); // El 10 indica que el valor es base 10
+
+        // Verificar que el valor de 'tamano' sea un número
+        if (isNaN(tamano) || tamano < 0) { // isNaN() verifica si el valor no es un número
+            console.error('El valor ingresado no es un número válido.');
+            return;
+        }
+
+        const $casillasMain = $('.casillasMain');
+        // Limpiar el contenido del div casillasMain
+        $casillasMain.innerHTML = '';
+
+        // Generar las casillas
+        for (let i = 0; i < tamano; i++) {
+            const casilla = document.createElement('input');
+            casilla.type = 'text';
+            casilla.className = 'casilla';
+            $casillasMain.appendChild(casilla);
+        }
     });
 });
