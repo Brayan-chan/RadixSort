7e08e03cd0f725ac82150f8f055a60370081970a
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2024-10-11 23:33:14 -0600

    Verificar la entrada de datos

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
-})
+    // Constante para traer elementos del DOM
+    const $ = el => document.querySelector(el);
+
+    // Verifica que el elemento con id 'tamanoArreglo' existe
+    const $tamanoArreglo = $('#tamanoArreglo');
+    if (!$tamanoArreglo) {
+        console.error('Elemento con id "tamanoArreglo" no encontrado.');
+        return;
+    }
+
+    // Agrega un listener para el evento 'input'
+    $tamanoArreglo.addEventListener('input', function() {
+        const $tamano = $tamanoArreglo.value;
+        console.log($tamano);
+    });
+});
